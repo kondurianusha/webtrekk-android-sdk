298336edee6806baa10c7352091094b96390de81
arsen.vartbaronov <arsen.vartbaronov@webtrekk.com>
2016-04-08 15:26:20 +0200

    build on Jenkins

---
+++ SDKTest/build_test.sh
@@ -0,0 +1,19 @@
+./gradlew assembleRelease
+./gradlew assembleDebug
+
+#do unit tests
+
+ #run emulator
+gnome-terminal -x sh -c "emulator -avd Nexus_S_API_21"
+ #wait untill emulator started
+sleep 30
+ #do test
+./gradlew cAT
+
+ #extract logs
+cd ../..
+adb logcat -d > all_log.txt
+adb logcat -d WebtrekkSDK:* *:S > webtrekk_log.txt
+
+ #kill emulator
+adb emu kill

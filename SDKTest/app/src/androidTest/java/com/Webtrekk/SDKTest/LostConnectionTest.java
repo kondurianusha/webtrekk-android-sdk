fe2885d69282d3235f79aeab53bc6bdd61c9a5dc
arsen.vartbaronov <arsen.vartbaronov@webtrekk.com>
2016-05-24 15:47:29 +0200

    PIX-150 Add lan disconnection test

---
+++ SDKTest/app/src/androidTest/java/com/Webtrekk/SDKTest/LostConnectionTest.java
@@ -0,0 +1,80 @@
+package com.Webtrekk.SDKTest;
+
+import com.webtrekk.webtrekksdk.Utils.WebtrekkLogging;
+import com.webtrekk.webtrekksdk.Webtrekk;
+
+import java.io.IOException;
+
+/**
+ * Created by vartbaronov on 24.05.16.
+ */
+public class LostConnectionTest  extends ActivityInstrumentationTestCase2Base<EmptyActivity> {
+
+    private Webtrekk mWebtrekk;
+    private static final int TRACKING_CALLS_STACK = 1000;
+
+
+    public LostConnectionTest() {
+        super(EmptyActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mWebtrekk = Webtrekk.getInstance();
+        mWebtrekk.initWebtrekk(mApplication, R.raw.webtrekk_config_connection_broken_request);
+        getActivity();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+        finishActivitySync(getActivity());
+        setActivity(null);
+    }
+
+    public void testLostConnection()
+    {
+        long messageReeivedCounter = mHttpServer.getCurrentRequestNumber();
+
+        initWaitingForTrack(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < TRACKING_CALLS_STACK; i++)
+                {
+                    mWebtrekk.track();
+                }
+            }
+        });
+
+        //Wait for some message starts to send.
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            WebtrekkLogging.log("Sleep interruction");
+        }
+
+        WebtrekkLogging.log("Stop HTTP Server");
+        //stop http server - emulator connection brakes
+        mHttpServer.stop();
+
+        //wait sometime
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            WebtrekkLogging.log("Sleep interruction");
+        }
+
+        initWaitingForTrack (null, TRACKING_CALLS_STACK - (mHttpServer.getCurrentRequestNumber() - messageReeivedCounter));
+
+        try {
+            mHttpServer.start();
+            WebtrekkLogging.log("Start HTTP Server");
+        } catch (IOException e) {
+            WebtrekkLogging.log("testLostConnection. Can't start server one more time");
+        }
+
+        mWaitMilliseconds = 12000;
+        waitForTrackedURLs();
+    }
+}

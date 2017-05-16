97ab220b7b4c4bd1037d874a9574c84c95af696a
arsen.vartbaronov <arsen.vartbaronov@webtrekk.com>
2017-05-16 12:23:21 +0200

    Publish Android SDK
    Licenses are added to each file
    
    MT-1289

--- SDKTest/app/src/main/java/com/Webtrekk/SDKTest/PushNotificationActivity.java
+++ SDKTest/app/src/main/java/com/Webtrekk/SDKTest/PushNotificationActivity.java
@@ -1,3 +1,21 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Webtrekk GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Created by Arsen Vartbaronov on 16.03.16.
+ */
+
 package com.Webtrekk.SDKTest;
 
 import android.app.Activity;
@@ -29,9 +47,6 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-/**
- * Created by vartbaronov on 16.03.16.
- */
 public class PushNotificationActivity extends Activity {
 
     private volatile boolean mUseURLLink;
--- WebtrekkSDK/WebtrekkSDK/src/main/java/com/webtrekk/webtrekksdk/TrackingParameter.java
+++ WebtrekkSDK/WebtrekkSDK/src/main/java/com/webtrekk/webtrekksdk/TrackingParameter.java
@@ -1,3 +1,20 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Webtrekk GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Created by Thomas Dahlmann on 11.03.15.
+ */
 package com.webtrekk.webtrekksdk;
 
 import com.webtrekk.webtrekksdk.Utils.WebtrekkLogging;
@@ -7,13 +24,12 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 /**
- * Created by user on 11/03/15.
- *
  * this class deals with all the possible tracking parameters, it offers a static enum for all the valid ones
  * it also allows to return the trackingParameter as url, and maybe later as json as well
  * it offers the user some helper functions for easy of use, and will be created before a tracking request is send
  * the manual information has to be added by the user, the automatic information will be added by the sdk
  */
+
 public class TrackingParameter {
     // general tracking trackingParameter
     private SortedMap<Parameter, String> defaultParameter;
